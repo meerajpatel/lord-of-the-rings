cf368de8700a7ca05f4a106c39a8df5edde2e831
Meeraj <meeraj.patel@bizongo.com>
2021-06-02 22:57:54 +0530

    Updated the home page as well removed unused dependency

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,10 +1,19 @@
 import { React } from 'react';
-import lotr from './lotr.png'
 
 const Home = () => {
     return (
         <>
-            <img src={lotr} alt="Logo" height="100%" width="100%" />
+            <div style={{ display: "flex", justifyContent: "center", fontSize: "40px", fontWeight: 600 }}>
+                HELLO LOTR FANS
+            </div>
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                marginTop: "10px",
+                fontSize: "20px"
+            }}>
+                Click here: &nbsp;<a href="https://github.com/meerajpatel2011/lord-of-the-rings" rel="noreferrer" target="_blank">Code Base</a> &nbsp;and&nbsp; <a href="https://github.com/meerajpatel2011/lord-of-the-rings/blob/master/README.md" rel="noreferrer" target="_blank">Read Me / Assumptions</a>
+        </div>
         </>
     );
 }
